fae1714c090f7663368ba1aa4953c101490f8ea1
lucas <lucas6eng@gmail.com>
2023-02-16 10:46:29 +0100

    Rename root directory

--- .dotfiles/.dotfiles-install.sh
+++ .dotfiles/.dotfiles-install.sh
@@ -1,6 +1,6 @@
-git clone --bare https://github.com/alexfertel/iamlucassantos.git $HOME/dotfiles
+git clone --bare https://github.com/alexfertel/iamlucassantos.git $HOME/.dotfiles
 function dotfiles {
-   /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 mkdir -p .dotfiles-backup
 dotfiles checkout
