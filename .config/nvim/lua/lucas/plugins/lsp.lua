88bab9b56e321e71c515e453ebdf8c574d996661
lucas <lucas6eng@gmail.com>
2023-12-28 23:12:58 +0100

    Make remap of lsp return correct map info

--- .config/nvim/lua/lucas/plugins/lsp.lua
+++ .config/nvim/lua/lucas/plugins/lsp.lua
@@ -39,52 +39,52 @@ return {
 				-- Go to definition
 				vim.keymap.set("n", "gd", function()
 					vim.lsp.buf.definition()
-				end, opts, { desc = "Go to definition" })
+				end, { desc = "Go to definition" }, opts)
 
 				-- Show hover information
 				vim.keymap.set("n", "K", function()
 					vim.lsp.buf.hover()
-				end, opts, { desc = "Show hover information" })
+				end, { desc = "Show hover information" })
 
 				-- Search workspace symbols
 				vim.keymap.set("n", "<leader>vws", function()
 					vim.lsp.buf.workspace_symbol()
-				end, opts, { desc = "Search workspace symbols" })
+				end, { desc = "Search workspace symbols" }, opts)
 
 				-- Show diagnostics in a floating window
 				vim.keymap.set("n", "<leader>vd", function()
 					vim.diagnostic.open_float()
-				end, opts, { desc = "Show diagnostics in floating window" })
+				end, { desc = "Show diagnostics in floating window" }, opts)
 
 				-- Go to next diagnostic
 				vim.keymap.set("n", "[d", function()
 					vim.diagnostic.goto_next()
-				end, opts, { desc = "Go to next diagnostic" })
+				end, { desc = "Go to next diagnostic" }, opts)
 
 				-- Go to previous diagnostic
 				vim.keymap.set("n", "]d", function()
 					vim.diagnostic.goto_prev()
-				end, opts, { desc = "Go to previous diagnostic" })
+				end, { desc = "Go to previous diagnostic" }, opts)
 
 				-- Show code actions
 				vim.keymap.set("n", "<leader>vca", function()
 					vim.lsp.buf.code_action()
-				end, opts, { desc = "Show code actions" })
+				end, { desc = "Show code actions" }, opts)
 
 				-- Show references
 				vim.keymap.set("n", "<leader>vrr", function()
 					vim.lsp.buf.references()
-				end, opts, { desc = "Show references" })
+				end, { desc = "Show references" }, opts)
 
 				-- Rename symbol
 				vim.keymap.set("n", "<leader>vrn", function()
 					vim.lsp.buf.rename()
-				end, opts, { desc = "Rename symbol" })
+				end, { desc = "Rename symbol" }, opts)
 
 				-- Show signature help in insert mode
 				vim.keymap.set("i", "<C-h>", function()
 					vim.lsp.buf.signature_help()
-				end, opts, { desc = "Show signature help in insert mode" })
+				end, { desc = "Show signature help in insert mode" }, opts)
 			end)
 
 			lsp_zero.extend_lspconfig()
