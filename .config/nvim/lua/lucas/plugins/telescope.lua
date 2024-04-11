a2ac8a290fd203d28846f745f978010ec026b9a3
Lucas Vieira dos Santos <lucas6eng@gmail.com>
2024-04-11 23:04:14 +0200

    Add trouble to telescope

--- .config/nvim/lua/lucas/plugins/telescope.lua
+++ .config/nvim/lua/lucas/plugins/telescope.lua
@@ -21,5 +21,21 @@ return {
 				desc = "Grep word under cursor",
 			},
 		},
+
+		config = function()
+			local actions = require("telescope.actions")
+			local trouble = require("trouble.providers.telescope")
+
+			local telescope = require("telescope")
+
+			telescope.setup({
+				defaults = {
+					mappings = {
+						i = { ["<c-t>"] = trouble.open_with_trouble },
+						n = { ["<c-t>"] = trouble.open_with_trouble },
+					},
+				},
+			})
+		end,
 	},
 }
