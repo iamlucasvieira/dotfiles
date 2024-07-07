196260d58fa5c2cbbebc6b603bc344d8ded429fa
Lucas Vieira dos Santos <lucas6eng@gmail.com>
2024-07-07 14:41:09 +0200

    Make linting ignore errors (need for when eslint not used)

--- .config/nvim/lua/lucas/plugins/lint.lua
+++ .config/nvim/lua/lucas/plugins/lint.lua
@@ -14,7 +14,7 @@ return {
 
 			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 				callback = function()
-					require("lint").try_lint()
+					require("lint").try_lint(nil, { ignore_errors = true })
 				end,
 			})
 
