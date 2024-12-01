78e6d4e1f5fb5a9f7e0e043930431e2edca1da9c
Lucas Vieira dos Santos <lucas6eng@gmail.com>
2024-12-01 16:02:54 +0100

    Update lsp to v4

--- .config/nvim/lua/lucas/plugins/lsp.lua
+++ .config/nvim/lua/lucas/plugins/lsp.lua
@@ -21,139 +21,92 @@ return {
 			})
 		end,
 	},
-
 	{
-		"williamboman/mason-lspconfig.nvim",
-	},
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
 		config = function()
-			local lsp_zero = require("lsp-zero")
+			local cmp = require("cmp")
 
-			lsp_zero.on_attach(function(client, bufnr)
-				local opts = { buffer = bufnr, remap = false }
-
-				-- Go to definition
-				vim.keymap.set("n", "gd", function()
-					vim.lsp.buf.definition()
-				end, { desc = "Go to definition" }, opts)
-
-				-- Show hover information
-				vim.keymap.set("n", "K", function()
-					vim.lsp.buf.hover()
-				end, { desc = "Show hover information" })
-
-				-- Search workspace symbols
-				vim.keymap.set("n", "<leader>vws", function()
-					vim.lsp.buf.workspace_symbol()
-				end, { desc = "Search workspace symbols" }, opts)
-
-				-- Show diagnostics in a floating window
-				vim.keymap.set("n", "<leader>vd", function()
-					vim.diagnostic.open_float()
-				end, { desc = "Show diagnostics in floating window" }, opts)
-
-				-- Go to next diagnostic
-				vim.keymap.set("n", "[d", function()
-					vim.diagnostic.goto_next()
-				end, { desc = "Go to next diagnostic" }, opts)
-
-				-- Go to previous diagnostic
-				vim.keymap.set("n", "]d", function()
-					vim.diagnostic.goto_prev()
-				end, { desc = "Go to previous diagnostic" }, opts)
-
-				-- Show code actions
-				vim.keymap.set("n", "<leader>vca", function()
-					vim.lsp.buf.code_action()
-				end, { desc = "Show code actions" }, opts)
-
-				-- Show references
-				vim.keymap.set("n", "<leader>vrr", function()
-					vim.lsp.buf.references()
-				end, { desc = "Show references" }, opts)
-
-				-- Rename symbol
-				vim.keymap.set("n", "<leader>vrn", function()
-					vim.lsp.buf.rename()
-				end, { desc = "Rename symbol" }, opts)
-
-				-- Show signature help in insert mode
-				vim.keymap.set("i", "<C-h>", function()
-					vim.lsp.buf.signature_help()
-				end, { desc = "Show signature help in insert mode" }, opts)
-			end)
-
-			lsp_zero.extend_lspconfig()
-			require("mason").setup({})
-			require("mason-lspconfig").setup({
-				ensure_installed = { "gopls", "pyright" },
-				handlers = {
-					lsp_zero.default_setup,
-					lua_ls = function()
-						local lua_opts = lsp_zero.nvim_lua_ls()
-						require("lspconfig").lua_ls.setup(lua_opts)
+			cmp.setup({
+				sources = {
+					{ name = "nvim_lsp" },
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+				}),
+				snippet = {
+					expand = function(args)
+						vim.snippet.expand(args.body)
 					end,
 				},
 			})
-			local on_attach = function(client, bufnr)
-				if client.name == "ruff_lsp" then
-					-- Disable hover in favor of Pyright
-					client.server_capabilities.hoverProvider = false
-				end
-			end
+		end,
+	},
 
-			require("lspconfig").ruff_lsp.setup({
-				on_attach = on_attach,
+	{
+		"neovim/nvim-lspconfig",
+		cmd = { "LspInfo", "LspInstall", "LspStart" },
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+		init = function()
+			-- Reserve a space in the gutter
+			-- This will avoid an annoying layout shift in the screen
+			vim.opt.signcolumn = "yes"
+		end,
+		config = function()
+			local lsp_defaults = require("lspconfig").util.default_config
+
+			-- Add cmp_nvim_lsp capabilities settings to lspconfig
+			-- This should be executed before you configure any language server
+			lsp_defaults.capabilities =
+				vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+			-- LspAttach is where you enable features that only work
+			-- if there is a language server active in the file
+			vim.api.nvim_create_autocmd("LspAttach", {
+				desc = "LSP actions",
+				callback = function(event)
+					local opts = { buffer = event.buf }
+
+					vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+					vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+					vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+					vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+					vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+					vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+					vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+					vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+					vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+					vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+				end,
 			})
 
-			require("lspconfig").pyright.setup({
-				settings = {
-					pyright = {
-						-- Using Ruff's import organizer
-						disableOrganizeImports = true,
-					},
-					python = {
-						analysis = {
-							-- Ignore all files for analysis to exclusively use Ruff for linting
-							ignore = { "*" },
-						},
-					},
+			require("mason-lspconfig").setup({
+				ensure_installed = {},
+				handlers = {
+					-- this first function is the "default handler"
+					-- it applies to every language server without a "custom handler"
+					function(server_name)
+						require("lspconfig")[server_name].setup({})
+					end,
 				},
 			})
 		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
 	},
 	{
 		"neovim/nvim-lspconfig",
 	},
 	{
 		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local lsp_zero = require("lsp-zero")
-			lsp_zero.extend_cmp()
-
-			local cmp = require("cmp")
-			local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-			cmp.setup({
-				sources = {
-					{ name = "path" },
-					{ name = "nvim_lsp" },
-					{ name = "nvim_lua" },
-				},
-				formatting = lsp_zero.cmp_format(),
-				mapping = cmp.mapping.preset.insert({
-					["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-					["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete(),
-				}),
-			})
-		end,
 	},
 	{
 		"L3MON4D3/LuaSnip",
